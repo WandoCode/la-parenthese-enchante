435b01f9915ec725259370a44b89dfe9fb133c8f
wandole <mchirez@gmail.com>
2023-07-17 18:33:19 +0200

    Initial commit

---
+++ components/header.js
@@ -0,0 +1,15 @@
+const template = `
+    <header>
+      <button class="burger-menu"></button>
+      <div class="logo"></div>
+      <nav id="main-nav">
+        <ul>
+          <li>Accueil</li>
+          <li>Les massages</li>
+          <li>Réservation</li>
+          <li>Qui suis-je?</li>
+          <li>Infos pratiques</li>
+        </ul>
+      </nav>
+    </header>
+`
