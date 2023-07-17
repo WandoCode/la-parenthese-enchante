56cf97c631e95d7330caf87b716015aa4f583d81
wandole <mchirez@gmail.com>
2023-07-17 18:53:35 +0200

    Add static content and text for header

--- components/header.js
+++ components/header.js
@@ -1,7 +1,14 @@
-const template = `
+const template = document.createElement('template')
+
+template.innerHTML = `
     <header>
-      <button class="burger-menu"></button>
-      <div class="logo"></div>
+      <button class="burger-menu">
+        <img class="burger-menu_img" src=''/>
+      </button>
+      <div class="logo">
+      <img class="logo_img" src=''/>
+      <p>La parenthèse enchantée</p>
+      </div>
       <nav id="main-nav">
         <ul>
           <li>Accueil</li>
@@ -17,6 +24,13 @@ const template = `
 export default class Header extends HTMLElement {
   constructor() {
     super()
-    this.innerHTML = template
+    this.attachShadow({ mode: 'open' })
+    this.shadowRoot.appendChild(template.content.cloneNode(true))
+
+    const burgerBtnImg = this.shadowRoot.querySelector('.burger-menu_img')
+    burgerBtnImg.src = '../public/burger_menu.svg'
+
+    const logoImg = this.shadowRoot.querySelector('.logo_img')
+    logoImg.src = '../public/logo_sm.png'
   }
 }
